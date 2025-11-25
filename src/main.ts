68736d801033c700ca490b54fbe0ae73a5d78c80
Karl Mårtensson <karlmartenssonsales@gmail.com>
2025-11-25 11:00:20 +0100

    added files + structure to the project

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,15 @@ const button = document.querySelector<HTMLButtonElement>(".goToPage")!;
 button.addEventListener("click", () => {
   window.location.href = "src/assets/html/product-page.html";
 });
+
+const button2 = document.querySelector<HTMLButtonElement>(".goToPage2")!;
+
+button2.addEventListener("click", () => {
+  window.location.href = "src/assets/html/checkout-page.html";
+});
+
+const button3 = document.querySelector<HTMLButtonElement>(".goToPage3")!;
+
+button3.addEventListener("click", () => {
+  window.location.href = "src/assets/html/order-complete-page.html";
+});
