712276500632aac8e3f6ace4200b63f9a24345ba
Karl Mårtensson <karlmartenssonsales@gmail.com>
2025-11-25 10:27:44 +0100

    added files and structure

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
+import "bootstrap/dist/css/bootstrap.css";
 import "./style.css";
+
+const button = document.querySelector<HTMLButtonElement>(".goToPage")!;
+
+button.addEventListener("click", () => {
+  window.location.href = "src/assets/html/product-page.html";
+});
