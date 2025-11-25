0c975ae367cfce319616fbb694340a4ac5fe9468
Karl Mårtensson <karlmartenssonsales@gmail.com>
2025-11-25 19:25:35 +0100

    added back button and new logotype with <a> connected to it takes you to starting page

--- src/assets/ts/product-page.ts
+++ src/assets/ts/product-page.ts
@@ -1,3 +1,8 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "../css/product-page.css";
+import { arrowLeft } from "./selector";
+
+arrowLeft!.addEventListener("click", () => {
+  window.location.href = "/";
+});
